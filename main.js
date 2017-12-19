1f2d856659cdded6459983b77b166b4aa26f37f2
Leonardo Matos <leomp120894@gmail.com>
2017-12-18 22:40:40 -0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let json = {}
 let str = ''
 
 function escapeStr (str) {
-  return str.replace(/([\S]+_[\S]+)/i, '`$1`').replace('+', '`+`')
+  return str.replace(/([^\s,]+_[^\s,]+)/i, '`$1`').replace('+', '`+`')
 }
 
 function strX (str, json, spaces) {
